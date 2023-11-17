7180c2a9dd457018e120e70b739f7d3f1aabfb8e
Abhijeetwasu <141164722+Abhijeetwasu@users.noreply.github.com>
2023-11-16 18:12:55 -0800

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+const openWeatherMapAPIKey = "983dc274aec42fb5bb52778a7d2805d3"
+const city = "Toronto"
+
+type WeatherData struct {
+	City        string  `json:"city"`
+	Temperature string  `json:"temperature"`
+	Description string  `json:"description"`
+	Humidity    float64 `json:"humidity"`
+	WindSpeed   float64 `json:"wind_speed"`
+	WeatherIcon string  `json:"weather_icon"`
+}
+
+func kelvinToCelsius(kelvin float64) string {
+	celsius := kelvin - 273.15
+	return fmt.Sprintf("%.2f", celsius)
+}
 
 func weatherHandler(w http.ResponseWriter, r *http.Request) {
 
